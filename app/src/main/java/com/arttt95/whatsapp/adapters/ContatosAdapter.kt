6a2aa778231d489576b883f160fd35a172749fd0
Arthur Damasceno <thuurdamasceno@gmail.com>
2025-03-06 19:39:41 -0300

    06MAR25 - Aula 276 AS - Criando adapter para listagem de contatos (Adapter, RecyclerView, notifyDataSetChanged).

---
+++ app/src/main/java/com/arttt95/whatsapp/adapters/ContatosAdapter.kt
@@ -0,0 +1,56 @@
+package com.arttt95.whatsapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.arttt95.whatsapp.databinding.ItemContatosBinding
+import com.arttt95.whatsapp.models.Usuario
+import com.squareup.picasso.Picasso
+
+class ContatosAdapter: Adapter<ContatosAdapter.ContatosViewHolder> () { // Herda de androidx.recyclerview...
+
+    private var listaContatos = emptyList<Usuario>()
+
+    fun adicionarLista(lista: List<Usuario>) {
+
+        listaContatos = lista
+        notifyDataSetChanged()
+
+    }
+    
+    inner class ContatosViewHolder(
+        private val binding: ItemContatosBinding
+    ) : ViewHolder(binding.root) {
+
+        fun bind(usuario: Usuario) {
+
+            binding.textContatoNome.text = usuario.nome
+            Picasso.get()
+                .load(usuario.foto)
+                .into(binding.imgContatoFoto)
+
+        }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContatosViewHolder {
+
+        val inflater = LayoutInflater.from(parent.context) // Gerando um inflater a partir de um context do parent, nesse caso não temos um inflater e foi necessário criar
+        val itemView = ItemContatosBinding.inflate(
+            inflater, parent, false
+        )
+        return ContatosViewHolder(itemView)
+
+    }
+
+    override fun getItemCount(): Int {
+        return listaContatos.size
+    }
+
+    override fun onBindViewHolder(holder: ContatosViewHolder, position: Int) {
+        val usuario = listaContatos[position]
+        holder.bind(usuario)
+    }
+}
--- app/src/main/java/com/arttt95/whatsapp/fragments/ContatosFragment.kt
+++ app/src/main/java/com/arttt95/whatsapp/fragments/ContatosFragment.kt
@@ -1,11 +1,15 @@
 package com.arttt95.whatsapp.fragments
 
+import android.icu.lang.UCharacter.VerticalOrientation
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.arttt95.whatsapp.adapters.ContatosAdapter
 import com.arttt95.whatsapp.databinding.FragmentContatosBinding
 import com.arttt95.whatsapp.models.Usuario
 import com.google.firebase.auth.FirebaseAuth
@@ -14,18 +18,19 @@ import com.google.firebase.firestore.ListenerRegistration
 
 class ContatosFragment : Fragment() {
 
-    private lateinit var binding: FragmentContatosBinding
+    private lateinit var binding: FragmentContatosBinding // BINDING
 
     private val firebaseAuth by lazy {
         FirebaseAuth.getInstance()
-    }
+    } // AUTH Reference
 
     private val firestore by lazy {
         FirebaseFirestore.getInstance()
-    }
+    } // FIRESTORE Reference
 
     private lateinit var eventoSnapshot: ListenerRegistration
     private val idUsuarioLogado = firebaseAuth.currentUser?.uid
+    private lateinit var contatosAdapter: ContatosAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,6 +43,18 @@ class ContatosFragment : Fragment() {
             container,
             false
         )
+
+        // Criando o Adapter de consumo para o RecyclerView rvContatos
+        contatosAdapter = ContatosAdapter()
+        binding.rvContatos.adapter = contatosAdapter
+        binding.rvContatos.layoutManager = LinearLayoutManager(context)
+        // Criando a divisória entre os items (Vertical)
+        binding.rvContatos.addItemDecoration(
+            DividerItemDecoration(
+                context, LinearLayoutManager.VERTICAL
+            )
+        )
+
         return binding.root
 
         // MANEIRA convencional de fazer o inflate do Fragment sem o binding
@@ -68,13 +85,17 @@ class ContatosFragment : Fragment() {
 
                     val usuario = documentSnapshot.toObject( Usuario::class.java )
 
-                    if(usuario != null) {
-//                        Log.i("fragment_contatos", "Nome: ${usuario.nome} ")
-                        if(idUsuarioLogado != null) {
-                            if(idUsuarioLogado != usuario.id) {
-                                listaContatos.add(usuario)
-                                Log.i("fragment_contatos", "Nome: ${usuario.nome} ")
-                            }
+                    if(usuario != null && idUsuarioLogado != null) {
+
+                        // Log.i("fragment_contatos", "Nome: ${usuario.nome} ")
+
+                        if(idUsuarioLogado != usuario.id) { // Separa o currentUser
+
+                            listaContatos.add(usuario)
+
+                            // Deve imprimir apenas usuário que não são o currentUser
+                            Log.i("fragment_contatos", "Nome: ${usuario.nome} ")
+
                         }
 
                     }
@@ -82,6 +103,11 @@ class ContatosFragment : Fragment() {
                 }
 
                 // Lista de contatos vai servir para alimentar o RecyclerView
+                if(listaContatos.isNotEmpty()) {
+
+                    contatosAdapter.adicionarLista( listaContatos )
+
+                }
 
             }
 
